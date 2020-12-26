c59a31e566a6186e9a7b247e163d05c7c933e8a4
RenoFar <renaudfaraci@gmail.com>
2020-12-26 18:46:54 +0100

    create metho requestApi.js

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,11 +1,14 @@
-let imageBestMovie = document.getElementsByClassName("section__rightHero");
-
-imageBestMovie[0].style.backgroundImage = "url('imageTest.jpg')"; 
-
-
 let btnLeft = document.getElementsByClassName("arrow--left")[0];
 
 btnLeft.addEventListener("click", function(event){
     event.preventDefault();
-    console.log("ok");
+    // console.log("ok");
 } );
+
+let btnright = document.getElementsByClassName("arrow--right")[0];
+
+btnright.addEventListener("click", function(event){
+    event.preventDefault();
+    // console.log("ok");
+} );
+
--- js/requestApi.js
+++ js/requestApi.js
@@ -1,28 +1,100 @@
-let resultApi = []
+let resultApi = [];
 
-let requestFirstBest = new XMLHttpRequest();
-requestFirstBest.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        let response = JSON.parse(this.responseText);
-        for (let i=0; i<5; i++){
+const urlRequest = [
+    'http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
+    'http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Action&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
+    'http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Animation&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
+    'http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=History&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains='
+];
+
+// fetch("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=")
+// .then(function(response){
+//     return response.json();
+// })
+// .then(function(response){
+//     for (let i=0; i<5; i++){
+//         resultApiBestMovies.push(response.results[i]);
+//     }
+
+const requestApiCategory = (urlRequest, elementQty) => {
+    fetch(urlRequest)
+    .then(response => response.json())
+    .then(function(response){
+        console.log("jsonified: "+ response);
+        for (let i=0; i<elementQty; i++){
             resultApi.push(response.results[i]);
         }
-        console.log(resultApi);
-    }
+        return;
+    });
+    return new Promise(function(resolve, reject){
+        resolve();
+    });
 };
-requestFirstBest.open("GET", "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=");
-requestFirstBest.send();
 
-let requestLastBest = new XMLHttpRequest();
-requestLastBest.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        let response = JSON.parse(this.responseText);
-        for (let i=0; i<2; i++){
-            resultApi.push(response.results[i]);
-        }
-        console.log(resultApi);
+const fillImageCategory = (category) => {
+    let imageCategory = document.querySelectorAll(category);
+    let imageNumber = 6;
+    if (category == "#bestMovie div.section__rightHero") {
+        imageNumber = 0;
     }
+    console.log("imageNumber " + imageNumber);
+    while (imageNumber>=0){
+        imageCategory[imageNumber].style.backgroundImage = `url(${resultApiBestMovie[imageNumber].image_url})`; 
+        imageNumber--;
+    };
 };
-requestLastBest.open("GET", "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=&page=2");
-requestLastBest.send();
 
+const main = () => {
+    for (let i=0; i<urlRequest.length; i++){
+        requestApiCategory(urlRequest[i], 5)
+        .then(function(response){
+            requestApiCategory(urlRequest[i] + "&page=2", 2);
+            return;
+        });
+    };
+};
+
+main();
+
+// let resultApiBestMovies = [];
+// let resultApiFirstCat = [];
+// let resultApiSecondCat = [];
+// let resultApiThirdCat = [];
+
+// fetch("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=")
+// .then(function(response){
+//     return response.json();
+// })
+// .then(function(response){
+//     for (let i=0; i<5; i++){
+//         resultApiBestMovies.push(response.results[i]);
+//     }
+//     console.log("resultApiBestMovies" + resultApiBestMovies);
+//     return;
+// })
+// .then(function(){
+//     fetch("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=&page=2")
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(response){
+//         for (let i=0; i<2; i++){
+//             resultApiBestMovies.push(response.results[i]);
+//         }
+//         console.log("resultApiBestMovies" + resultApiBestMovies);
+//         fillImage();
+//         return;
+//     })
+// });
+
+// const fillImage = () => {
+//     let imageBestMovie = document.getElementsByClassName("section__rightHero");
+//     imageBestMovie[0].style.backgroundImage = `url(${resultApiBestMovies[0].image_url})`; 
+//     let imageBestRating = document.querySelectorAll("#bestRating div.tiles__tile img");
+//     console.log("imageBestRating" + imageBestRating);
+//     let i = 0;
+//     while (i<7){
+//         imageBestRating[i].style.backgroundImage = `url(${resultApiBestMovies[i].image_url})`; 
+//         i++;
+//     };
+// };
