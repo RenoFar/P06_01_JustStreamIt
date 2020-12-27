f17d862bf9661b86d62bb5ac4fa5566d557e9728
RenoFar <renaudfaraci@gmail.com>
2020-12-27 18:38:23 +0100

    add onclick functions

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,28 +1,54 @@
-for (i=0; i<4; i++) {
-    document.getElementsByClassName("arrow--left")[i]
-    .addEventListener("click", function(event){
-        event.preventDefault();
-        showTilesLeft(i);
-    });
-    document.getElementsByClassName("arrow--right")[i]
-    .addEventListener("click", function(event){
-        event.preventDefault();
-        showTilesRight(i);
-    });
+const showTilesRight = (cat) => {
+    console.log("showTilesRight");
+    let tiles = document.querySelectorAll(cat + ' div.tiles-tile');
+    for(i = 0; i < tiles.length; i++){
+        if(i < 3){
+            tiles[i].style.display = "none";
+        }
+        if(i > 4){
+            tiles[i].style.display = "flex";
+        }
+    }
 }
 
-const showTilesLeft = (i) => {
-    let tilesLeft = document
-    .getElementsByClassName("section--category")[i]
-    .getElementsByClassName("imageModal")[6];
-    tilesLeft.style.width = "0%";
-};
+const showTilesLeft = (cat) => {
+    console.log("showTilesLeft");
+    let tiles = document.querySelectorAll(cat + ' div.tiles-tile');
+    for(i = 0; i < tiles.length; i++){
+        if(i < 3){
+            tiles[i].style.display = "flex";
+        }
+        if(i > 4){
+            tiles[i].style.display = "none";
+        }
+    }
+}
 
-const showTilesRight = (i) => {
-    let tilesRight = document
-    .getElementsByClassName("section--category")[i]
-    .getElementsByClassName("imageModal")[6];
-    tilesRight.style.width = "100%";
+// for (i=0; i<4; i++) {
+//     document.getElementsByClassName("arrow--left")[i]
+//     .addEventListener("click", function(event){
+//         event.preventDefault();
+//         showTilesLeft(i);
+//     });
+//     document.getElementsByClassName("arrow--right")[i]
+//     .addEventListener("click", function(event){
+//         event.preventDefault();
+//         showTilesRight(i);
+//     });
+// }
+
+// const showTilesLeft = (i) => {
+//     let tilesLeft = document
+//     .getElementsByClassName("section--category")[i]
+//     .getElementsByClassName("imageModal")[6];
+//     tilesLeft.style.width = "0%";
+// };
+
+// const showTilesRight = (i) => {
+//     let tilesRight = document
+//     .getElementsByClassName("section--category")[i]
+//     .getElementsByClassName("imageModal")[6];
+//     tilesRight.style.width = "100%";
 
 
 // let btnLeft = document.getElementsByClassName("arrow--left");
