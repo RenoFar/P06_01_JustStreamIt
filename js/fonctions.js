7b33e9820652fe659ef2cab04d5dc3afe401196a
RenoFar <renaudfaraci@gmail.com>
2020-12-28 14:18:05 +0100

    upgrading slide

--- js/fonctions.js
+++ js/fonctions.js
@@ -3,10 +3,12 @@ const showTilesRight = (cat) => {
     let tiles = document.querySelectorAll(selectCat);
     for(i = 0; i < tiles.length; i++){
         if(i < 3){
-            tiles[i].style.display = "none";
+            tiles[i].classList.add("slideDown");
+            // tiles[i].style.display = "none";
         }
-        if(i > 4){
-            tiles[i].style.display = "flex";
+        if(i > 3){
+            tiles[i].classList.remove("slideDown");
+            // tiles[i].style.display = "flex";
         }
     }
 }
@@ -16,48 +18,12 @@ const showTilesLeft = (cat) => {
     let tiles = document.querySelectorAll(selectCat);
     for(i = 0; i < tiles.length; i++){
         if(i < 3){
-            tiles[i].style.display = "flex";
+            tiles[i].classList.remove("slideDown");
+            // tiles[i].style.display = "flex";
         }
-        if(i > 4){
-            tiles[i].style.display = "none";
+        if(i > 3){
+            tiles[i].classList.add("slideDown");
+            // tiles[i].style.display = "none";
         }
     }
 }
-
-// for (i=0; i<4; i++) {
-//     document.getElementsByClassName("arrow--left")[i]
-//     .addEventListener("click", function(event){
-//         event.preventDefault();
-//         showTilesLeft(i);
-//     });
-//     document.getElementsByClassName("arrow--right")[i]
-//     .addEventListener("click", function(event){
-//         event.preventDefault();
-//         showTilesRight(i);
-//     });
-// }
-
-// const showTilesLeft = (i) => {
-//     let tilesLeft = document
-//     .getElementsByClassName("section--category")[i]
-//     .getElementsByClassName("imageModal")[6];
-//     tilesLeft.style.width = "0%";
-// };
-
-// const showTilesRight = (i) => {
-//     let tilesRight = document
-//     .getElementsByClassName("section--category")[i]
-//     .getElementsByClassName("imageModal")[6];
-//     tilesRight.style.width = "100%";
-
-
-// let btnLeft = document.getElementsByClassName("arrow--left");
-
-// let btnright = document.getElementsByClassName("arrow--right");
-
-    // const showTilesright = () => {
-//     btnright.addEventListener("click", function(event){
-//         event.preventDefault();
-//         console.log("ok");
-//     } );
-// }
