72ad39f4e7de2058efd71040563314111853798c
RenoFar <renaudfaraci@gmail.com>
2020-12-29 00:42:13 +0100

    adding all categories

--- js/requestApi.js
+++ js/requestApi.js
@@ -1,12 +1,20 @@
 const urlRequest = [
-    ['http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
-    'http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=&page=2'],
-    ['http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Action&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
-    'http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Action&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=&page=2'],
-    ['http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Animation&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
-    'http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=Animation&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=&page=2'],
-    ['http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=History&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=',
-    'http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=History&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=&page=2']
+    ['http://localhost:8000/api/v1/titles/?sort_by=-imdb_score',
+    'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page=2'],
+    ['http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Action',
+    'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Action&page=2'],
+    ['http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Animation',
+    'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Animation&page=2'],
+    ['http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=History',
+    'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=History&page=2']
+];
+
+const categoryList = [
+    "#bestRating .imageModal",
+    "#categoryOne .imageModal",
+    "#categoryTwo .imageModal",
+    "#categoryThree .imageModal",
+    "#bestMovie .section__rightHero",
 ];
 
 let resultApi = [
@@ -16,65 +24,151 @@ let resultApi = [
     [],
 ];
 
-const loopApiRequest = () => {
-    return new Promise(resolve => {
-        let i = 0;
-        while (i<urlRequest.length){
-            requestApiCategory(urlRequest[i][0], i, 5)
-            .then(requestApiCategory(urlRequest[i][1], i, 2));
-            i++;
-            if (i === (urlRequest.lengt -1)){
-                resolve();
-            }
-        }
+function fillImageCat(categorylist, category) {
+    console.log("fillImageCat start");
+    let imageCategory = document.querySelectorAll(categorylist);
+    console.log("imageCategory.lenght " + imageCategory.length);
+    let imageNumber = 6;
+    if (categorylist == "#bestMovie .section__rightHero") {
+        imageCategory[0].style.backgroundImage = `url(${resultApi[0][0].image_url})`;
     }
-)};
+    console.log("category " + category);
+    while (imageNumber>=0){
+        imageCategory[imageNumber].style.backgroundImage = `url(${resultApi[category][imageNumber].image_url})`;
+        console.log("resultApi[category][imageNumber].image_url : " + resultApi[category][imageNumber].image_url)
+        imageNumber--;
+    };
+    console.log("fillImageCategory end");
+};
 
-const requestApiCategory = (urlRequest, index, elementQty) => {
-    return new Promise(resolve => {
+function loopFillImageCat() {
+    console.log("loopFillImageCat start");
+    console.log("categoryList.length: " + categoryList.length)
+    for (let category=0; category<categoryList.length; category++){
+        fillImageCat(categoryList[category], category);
+    }
+};
+
+function requestApi(urlRequest, index, elementQty) {
+        console.log("requestApi start");
+        console.log("requestApi - index & elmtQty :" + index + " & " + elementQty);
         fetch(urlRequest)
-        .then(response => response.json())
-        .then(response => {
-            for (let i=0; i<elementQty; i++){
-                resultApi[index].push(response.results[i]);
-                console.log("index & i: " + index + " " + i);
-            };            
-        })
-        resolve();
-    })
+            .then(response => response.json())
+            // .then(function(response) { return response.json()})
+            .then(function(jsonResponse) {
+                console.log("jsonResponse " + jsonResponse);
+                for (let element=0; element<elementQty; element++){
+                    console.log("element: " + element);
+                    resultApi[index].push(jsonResponse.results[element]);
+                    }
+            })
+        console.log("requestApiCategory end");
 };
 
-const fillImageCategory = (category) => {
-    return new Promise(resolve => {
-        let imageCategory = document.querySelectorAll(category);
-        console.log("imageCategory.lenght " + imageCategory.length);
-        let imageNumber = 6;
-        if (category == "#bestMovie .section__rightHero") {
-            imageNumber = 0;
-        }
-        console.log("imageNumber " + imageNumber);
-        while (imageNumber>=0){
-            imageCategory[imageNumber].style.backgroundImage = `url(${resultApi[0][imageNumber].image_url})`; /* ${resultApi[0][imageNumber].image_url} */
-            imageNumber--;
-        };
-        resolve();
-    })
+function loopApiRequest() {
+    console.log("loopApiRequest start");
+    console.log("urlRequest.length: " + urlRequest.length)
+    for (let i=0; i<urlRequest.length; i++){
+        requestApi(urlRequest[i][0], i, 5);
+        requestApi(urlRequest[i][1], i, 2);
+    }
 };
 
-const main=() => {
-    loopApiRequest()
-    .then(fillImageCategory("#bestMovie .section__rightHero"))
-    .then(fillImageCategory("#bestRating .tiles__tile"));
+function main() {
+    loopApiRequest();
+    // while (true) {
+    // if (resultApi[0].length == 7 && resultApi[1].length == 7 &&
+    //     resultApi[2].length == 7 && resultApi[3].length == 7){
+    //         break
+    //     }
+    // }
+    setTimeout(() => loopFillImageCat(), 900);
+    // loopFillImageCat();
 };
 
 main();
+
+
+// const loopApiRequest = () => {
+//     return new Promise(resolve => {
+//         console.log("loopApiRequest start");
+//         console.log("urlRequest.length: " + urlRequest.length)
+//         for (let i=0; i<urlRequest.length; i++){
+//             requestApiCategory(urlRequest[i][0], i, 5)
+//                 .then(() => {
+//                     requestApiCategory(urlRequest[i][1], i, 2)
+//                 })
+//                 .then(() => {
+//                     console.log("i++ " + i);
+//                     if (i==urlRequest.length-1){
+//                         resolve;
+//                         console.log("loopApiRequest resolve ok");
+//                     }
+//                 });
+//         }
+//         console.log("loopApiRequest end");
+//     })
+// };
+
+// const requestApiCategory = (urlRequest, index, elementQty) => {
+//     return new Promise(resolve => {
+//         console.log("requestApiCategory start");
+//         console.log("requestApiCategory - index & elmtQty :" + index + " & " + elementQty);
+//         fetch(urlRequest)
+//             .then(response => response.json())
+//             .then(jsonResponse => {
+//                 for (let i=0; i<elementQty; i++){
+//                     resultApi[index].push(jsonResponse.results[i]);
+//                     console.log("index & i: " + index + " " + i);
+//                     console.log("resultApi[index].push(jsonResponse.results[i]) : " 
+//                     + resultApi[index]);
+//                     if (i==elementQty-2){
+//                         resolve;
+//                         console.log("requestApiCategory resolve ok");
+//                     }
+//                 };            
+//             })
+//         console.log("requestApiCategory end");
+//     });
+// };
+
+// const fillImageCategory = (category) => {
+//         console.log("fillImageCategory start");
+//         let imageCategory = document.querySelectorAll(category);
+//         console.log("imageCategory.lenght " + imageCategory.length);
+//         let imageNumber = 6;
+//         if (category == "#bestMovie .section__rightHero") {
+//             imageNumber = 0;
+//         }
+//         console.log("imageNumber " + imageNumber);
+//         while (imageNumber>=0){
+//             imageCategory[imageNumber].style.backgroundImage = `url(${resultApi[0][imageNumber].image_url})`;
+//             console.log("resultApi[0][imageNumber].image_url : " + resultApi[0][imageNumber].image_url)
+//             imageNumber--;
+//         };
+//         console.log("fillImageCategory end");
+// };
+
+// const main = () => {    
+    // loopApiRequest()
+//         .then(() => {
+//             fillImageCategory("#bestMovie .section__rightHero")
+//             fillImageCategory("#bestRating .tiles__tile")
+//         });
+// };
+
+// if (document.readyState === "complete" || document.readyState === "interactive") {
+    // call on next available tick
+    // main();
+// }
+
 
 // let resultApiBestMovies = [];
 // let resultApiFirstCat = [];
 // let resultApiSecondCat = [];
 // let resultApiThirdCat = [];
 
-// fetch("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=")
+// fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
 // .then(function(response){
 //     return response.json();
 // })
@@ -82,11 +176,10 @@ main();
 //     for (let i=0; i<5; i++){
 //         resultApiBestMovies.push(response.results[i]);
 //     }
-//     console.log("resultApiBestMovies" + resultApiBestMovies);
 //     return;
 // })
 // .then(function(){
-//     fetch("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=&page=2")
+//     fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page=2")
 //     .then(function(response){
 //         return response.json();
 //     })
@@ -94,7 +187,6 @@ main();
 //         for (let i=0; i<2; i++){
 //             resultApiBestMovies.push(response.results[i]);
 //         }
-//         console.log("resultApiBestMovies" + resultApiBestMovies);
 //         fillImage();
 //         return;
 //     })
